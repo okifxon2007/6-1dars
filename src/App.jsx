991f75fe553e6e8b8cdce475b5a0a600f7c8b725
unknown <shokirjonovokifxon46@gmail.com>
2024-07-26 17:00:30 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from './components/Navbar/Navbar'
+import Main from './components/Main/index'
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Main />
+    </div>
+  )
+}
+
+export default App
